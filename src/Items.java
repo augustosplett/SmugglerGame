3510e1f40c66298a3c545d36c84e58307f3db961
Augusto Madeira Splett <augusto.splett@gmail.com>
2023-09-14 09:53:30 -0400

    initial commit

---
+++ src/Items.java
@@ -0,0 +1,7 @@
+public class Items {
+
+    String name;
+    Double price;
+}
+//association -> the object is created outside the object
+//composition -> when the object is created inside another object
